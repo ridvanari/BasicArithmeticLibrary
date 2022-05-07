d25461fb471be39c13ce49dfd419fe41933faf77
Rıdvan <ridvanari@yahoo.com>
2022-05-08 00:31:26 +0300

    Test project has been added.

---
+++ BasicArithmeticLibrary.Test/ArithmeticUnitTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BasicArithmeticLibrary.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ArithmeticUnitTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
