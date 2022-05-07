c204d8417c979a2f91b8c42f17bcecc0f87c34fd
Rıdvan <ridvanari@yahoo.com>
2022-05-07 18:33:41 +0300

    Add project files.

---
+++ BasicArithmeticLibrary/BasicArithmeticLibrary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicArithmeticLibrary
+{
+    public class BasicArithmeticLibrary
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
